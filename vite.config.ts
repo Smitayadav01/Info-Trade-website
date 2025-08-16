ee3c85f6190074e86fad32680cef2a15e4fa7f0b
Smitayadav01 <960smitayadav@gmail.com>
2025-08-16 12:58:47 +0530

    commited

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+  build: {
+    outDir: 'dist'
+  },
+  server: {
+    port: 3000,
+    historyApiFallback: true, // 👈 no plugin needed
+  }
+})
