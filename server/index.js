e6c30f73164f3093e1aa0ef686b2c9360274b6af
Smitayadav01 <960smitayadav@gmail.com>
2025-09-06 14:14:49 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ["http://localhost:5173",process.env.FRONTEND_URL],
+  methods: ["GET", "POST"],
+  credentials: true
+}));
+
 app.use(express.json());
 
 // Create transporter for sending emails
