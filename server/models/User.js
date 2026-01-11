156239297d333ba92d242e13aaafd14b9a5fffb2
Smitayadav01 <960smitayadav@gmail.com>
2026-01-11 15:21:28 +0530

    commited

--- server/models/User.js
+++ server/models/User.js
@@ -40,10 +40,10 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre("save", async function (next) {
+userSchema.pre("save", async function () {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 12);
-  next();
+  
 });
 
 userSchema.methods.comparePassword = async function (candidatePassword) {
